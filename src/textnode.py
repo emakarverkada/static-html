c81ab8dc3e37c4f69273e08d66a352771c8a8c44
ebeerweb <erin.makarova@gmail.com>
2024-11-25 10:19:34 -0800

    splitting blocks

--- src/textnode.py
+++ src/textnode.py
@@ -58,7 +58,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(old_node)
             continue
         if delimiter not in old_node.text:
-            raise ValueError("Delimited not found in string")      
+            new_nodes.append(old_node)
+            continue
+            #raise ValueError("Delimited not found in string")      
         text = old_node.text.split(delimiter)
         if len(text) % 2 == 0:
             raise ValueError("Invalid markdown, formatted section not closed")
@@ -72,8 +74,13 @@ def split_nodes_image(old_nodes):
     pattern = r"!(\[[^\[\]]*\]\([^\(\)]*\))"
     new_nodes = []
     for old_node in old_nodes:
+        if old_node.text_type is not TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
         if not re.findall(pattern,old_node.text):
-            raise ValueError("Image not found in string")
+            #raise ValueError("Image not found in string")
+            new_nodes.append(old_node)
+            continue
         text = re.split(pattern, old_node.text)
         text[::2] = [TextNode(x, TextType.TEXT) for x in text[::2]]
         text[1::2] = [TextNode(re.findall(r"\[([^\[\]]*)\]\(([^\(\)]*)\)", x)[0][0], TextType.IMAGE, re.findall(r"\[([^\[\]]*)\]\(([^\(\)]*)\)", x)[0][1]) for x in text[1::2]]
@@ -85,11 +92,31 @@ def split_nodes_link(old_nodes):
     pattern = r"(?<!!)(\[[^\[\]]*\]\([^\(\)]*\))"
     new_nodes = []
     for old_node in old_nodes:
+        if old_node.text_type is not TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
         if not re.findall(pattern,old_node.text):
-            raise ValueError("Image not found in string")
+            #raise ValueError("Image not found in string")
+            new_nodes.append(old_node)
+            continue
         text = re.split(pattern, old_node.text)
         text[::2] = [TextNode(x, TextType.TEXT) for x in text[::2]]
         text[1::2] = [TextNode(re.findall(r"\[([^\[\]]*)\]\(([^\(\)]*)\)", x)[0][0], TextType.LINK, re.findall(r"\[([^\[\]]*)\]\(([^\(\)]*)\)", x)[0][1]) for x in text[1::2]]
         text = [x for x in text if x.text]
         new_nodes.extend(text)
     return new_nodes
+
+def text_to_textnodes(text):
+    text = [TextNode(text, TextType.TEXT)]
+    text = split_nodes_delimiter(text, "**", TextType.BOLD)
+    text = split_nodes_delimiter(text, "*", TextType.ITALIC)
+    text = split_nodes_delimiter(text, "`", TextType.CODE)
+    text = split_nodes_image(text)
+    text = split_nodes_link(text)
+    return text
+
+def markdown_to_blocks(text):
+    #text to blocks
+    blank_line_regex = r"(?:\r?\n){2,}"
+    return re.split(blank_line_regex, text.strip())
+
